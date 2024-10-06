b8307e457d0c2d534ec9999c2806f645a3ca1f60
鈴木隼人 <>
2024-10-07 08:18:04 +0900

    条件にあったデータだけを取得する方法
    Eloquent ORMをたくさん練習した

--- try3-step19/app/Http/Controllers/PostController.php
+++ try3-step19/app/Http/Controllers/PostController.php
@@ -9,6 +9,14 @@ class PostController extends Controller
 {
     public function index(){
         $posts=Post::all();
+        // $posts=Post::where('user_id', auth()->id())->get();  ログインユーザーの投稿データを取得
+        // $posts=Post::where('user_id', '!', auth()->id())->get();  ログインユーザー以外の投稿データーを取得
+        // $posts=Post::whereDate('created_at', '>=', '2024-10-04')->get(); 2024-10-04以降に作成された投稿データを取得
+        // $posts=Post::where('user_id', 1)->whereDate('created_at', '2024-10-04')->get(); user_idカラムが1でかつ、2024年10月4日以降の投稿データ取得
+        // 作成日を新しい順にしてデータを取得↓
+        $posts=Post::orderBy('created_at', 'desc')->get();
+        // $posts=Post::whereDate('created_at', '2024-10-04')->first();  作成日を新しい順にしてデータをひとつだけ取得する
+        // $posts=Post::find(1);  idが1のデータを取得する　　　foreach関数と一緒に使用すると、foreachを使用する必要がないため、エラーになる
         return view('post.index', compact('posts'));
     }
     public function create() {
